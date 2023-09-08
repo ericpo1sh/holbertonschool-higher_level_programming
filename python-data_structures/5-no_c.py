bc51d0fe92b6ccbd6c22714cc06902b6fe90cb62
ericpo1sh <ericpo1sh@gmail.com>
2023-09-08 12:06:26 -0700

    test

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     if my_string:
         for i in range(0, len(my_string)):
             if my_string[i] == "c" or my_string[i] == "C":
-                my_string[i] == ""
+                my_string == my_string[:i] + my_string[i+1:]
     return my_string
