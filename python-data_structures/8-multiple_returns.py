6282893eb21cc4a71e4f6bb49e4ae08431346178
ericpo1sh <ericpo1sh@gmail.com>
2023-09-08 20:13:16 -0700

    test

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,5 +2,5 @@ def multiple_returns(sentence):
     if sentence:
         newtuple = (len(sentence), sentence[0])
     else:
-        newtuple = (0, )
+        newtuple = (0, None)
     return newtuple
