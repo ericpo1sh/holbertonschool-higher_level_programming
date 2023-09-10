38f37afecc877e2cd322fbf6d1790349c5257264
ericpo1sh <ericpo1sh@gmail.com>
2023-09-10 12:38:11 -0700

    Write a function that adds all unique integers in a list (only once for each integer).

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    total = 0
-    for num in my_list:
-        if my_list.count(num) == 1:
-            total += num
-    return total
+    if my_list:
+        total = 0
+        for num in my_list:
+            if my_list.count(num) == 1:
+                total += num
+        return total
