cd2336cc2f0c3c2165939910a87003b6e89f6534
ericpo1sh <ericpo1sh@gmail.com>
2023-09-11 08:45:17 -0700

    Write a function that returns a set of common elements in two sets.

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    for item in set_1:
-        for items in set_2:
-            if item == items:
-                set_3 = item
-    return set_3
+    return set_1.intersection(set_2)
