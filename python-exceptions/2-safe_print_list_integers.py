df3f03e8b92b07729cdc6c1cc0f1ff96c73bea54
ericpo1sh <ericpo1sh@gmail.com>
2023-09-12 10:56:11 -0700

    Made changes to Exception types, Removed if conditional for X

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     elements = 0
-    if x <= len(my_list):
-        for i in range(x):
-            try:
-                print("{:d}".format(my_list[i]), end="")
-                elements += 1
-            except (ValueError, TypeError, Exception, IndexError):
-                continue
-        print()
-        return elements
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            elements += 1
+        except (TypeError, ValueError):
+            continue
+    print()
+    return elements
