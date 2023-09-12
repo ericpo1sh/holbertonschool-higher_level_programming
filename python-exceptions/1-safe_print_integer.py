c605156a3bfe0af49139ec574a3deebb18fb4ab4
ericpo1sh <ericpo1sh@gmail.com>
2023-09-12 09:51:03 -0700

    Fixed print statement

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     if isinstance(value, int):
         try:
-            print(value)
+            print("{:d}".format(value))
             return True
         except ValueError:
             return False
