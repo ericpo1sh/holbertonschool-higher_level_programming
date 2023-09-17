8aa3da1c6bcfe09bfdb7e9e576f7a38147dae43f
ericpo1sh <ericpo1sh@gmail.com>
2023-09-16 20:03:23 -0700

    Testing

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -10,10 +10,10 @@ def print_square(size):
     Raises: TypeError, ValueError.
 
     """
-    if size < 0:
-        raise ValueError("size must be >= 0")
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
     for i in range(size):
         for x in range(size):
             print("#", end="")
