3b5ad0df0aaaec8a408953ca11428d78ef786e6d
ericpo1sh <ericpo1sh@gmail.com>
2023-09-16 18:26:50 -0700

    checking for tests

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,12 +3,16 @@
 
 
 def say_my_name(first_name, last_name=""):
+    """ Checking if first name is a valid string """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
+    """ Checking if last name is a valid string """
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
+    """ Printing result """
     print("My name is {} {}".format(first_name, last_name))
     return
+
 
 if __name__ == "__main__":
     import doctest
