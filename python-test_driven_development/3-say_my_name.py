32d35c977b83500265c0cda31b42763ada440f25
ericpo1sh <ericpo1sh@gmail.com>
2023-09-16 19:10:26 -0700

    Write a function that prints My name is <first name> <last name>

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -11,7 +11,6 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("last_name must be a string")
     """ Printing result """
     print("My name is {} {}".format(first_name, last_name))
-    return
 
 
 if __name__ == "__main__":
