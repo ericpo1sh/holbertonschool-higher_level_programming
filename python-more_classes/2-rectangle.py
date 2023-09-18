b64183af2008434da544db3f51e33dc1519f3d33
ericpo1sh <ericpo1sh@gmail.com>
2023-09-18 11:57:26 -0700

    Write a class Rectangle that defines a rectangle by: (based on 1-rectangle.py)

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -45,7 +45,7 @@ class Rectangle:
 
     def perimeter(self):
         """ Returns the perimeter for the rectangle """
-        if height == 0 or width == 0:
+        if self.__height == 0 or self.__width == 0:
             return 0
         else:
             return (2 * self.__width) + (2 * self.__height)
