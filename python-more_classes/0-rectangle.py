a28fdfcaf513d9edcaa771115883c41e865489ca
ericpo1sh <ericpo1sh@gmail.com>
2023-09-18 11:16:27 -0700

    Write an empty class Rectangle that defines a rectangle:

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Creating an empty class called rectangle """
+
+
+class Rectangle:
+    """ Rectangle class initiated """
+    pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,1 @@
+# Learing about more classes
