a39a7085c8bf69be72628fba075fc6cbbee02fe3
ericpo1sh <ericpo1sh@gmail.com>
2023-09-18 09:01:24 -0700

    Unittest tests added

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -2,5 +2,24 @@ import unittest
 from max_integer import max_integer
 
 class test_max_integer(unittest.TestCase):
-    def test(self):
-# 
+    """ Test for “max at the end” exists """
+    def test_end(self):
+        self.assertEqual(max_integer([1, 2, 3]), 3)
+    """ Test for “max at the beginning” exists """
+    def text_start(self)
+        self.assertEqual(max_integer([3, 2, 1]), 3)
+    """ Test for “max in the middle” exists """
+    def test_middle(self)
+        self.assertEqual(max_integer([2, 3, 1]), 3)
+    """ test for “one negative number in the list” exists """
+    def test_one_negative(self)
+        self.assertEqual(max_integer([-1, 2, 3, 5]), 5)
+    """ Test for “only negative numbers in the list” exists """
+    def test_all_negative(self)
+        self.assertEqual(max_integer([-1, -2 , -3, -4]), -1)
+    """ Test for “list of one element” exists """
+    def test_one_int(self)
+        self.assertEqual(max_integer([80]), 80)
+    """ Test for “list is empty” exists """
+    def test_empty_list(self)
+        self.assertEqual(max_integer([]), None)
