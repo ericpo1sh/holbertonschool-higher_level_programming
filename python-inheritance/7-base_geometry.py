e6891b3e592b96402803fd0976c4c8ba3380db55
ericpo1sh <ericpo1sh@gmail.com>
2023-09-22 13:42:10 -0700

    pls work

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -5,6 +5,7 @@
 class BaseGeometry:
     """ Write a class BaseGeometry. """
     def area(self):
+        """ initializing area """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
