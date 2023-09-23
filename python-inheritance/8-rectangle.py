d87b345ae11cf66bb6e96f07ff76a9bceda3cab3
ericpo1sh <ericpo1sh@gmail.com>
2023-09-23 08:47:04 -0700

    test

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Rectangle Module """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Initializing the rectangle subclass """
+
+    def __init__(self, width, height):
+        """ Validating with integer_validator """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
