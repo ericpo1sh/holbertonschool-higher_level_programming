5edb09c009929583e8c0cfaf2332e08ace2bfe41
ericpo1sh <ericpo1sh@gmail.com>
2023-09-23 08:43:44 -0700

    test

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """ Validating with integer_validator """
-        super().integer_validator("width", width)
+        self.integer_validator("width", width)
         self.__width = width
-        super().integer_validator("height", height)
+        self.integer_validator("height", height)
         self.__height = height
