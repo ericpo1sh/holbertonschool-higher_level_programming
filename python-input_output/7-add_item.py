39bd987de320bd34f14add7adde0772b7f8b9dab
ericpo1sh <ericpo1sh@gmail.com>
2023-09-25 14:23:58 -0700

    test

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """ 7-add_iem.py Module """
-import sys
+from sys import argv
 
 
-save_to_json_file = \
-    __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = \
-    __import__('6-load_from_json_file').load_from_json_file
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
 
 try:
-    PythonList = load_from_json_file("add_item.json")
+    py_list = load("add_item.json")
 except:
-    Pythonlist = []
-save_to_json_file(PythonList + sys.argv[1:], "add_item.json")
+    py_list = []
+finally:
+    save(py_list + argv[1:], "add_item.json")
