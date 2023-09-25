53795f1c79711eca5315c1e656f25efcbd8d8a49
ericpo1sh <ericpo1sh@gmail.com>
2023-09-25 12:37:39 -0700

    test

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" 2-append_write Module """
+
+
+def append_write(filename="", text=""):
+    """ initializing the append_write function """
+    if not open(filename, 'r'):
+        return
+    
+    with open(filename, 'a') as f:
+        return f.write(text)
