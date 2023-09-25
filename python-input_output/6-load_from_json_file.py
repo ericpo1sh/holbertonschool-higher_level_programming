9ba767bb10a93db7499be96d7b66481820475a7d
ericpo1sh <ericpo1sh@gmail.com>
2023-09-25 13:50:59 -0700

    testing

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,4 +6,4 @@ import json
 def load_from_json_file(filename):
    """ initializing the function """
    with open(filename, 'r+') as f:
-      return json.load(f.read())
+      return json.loads(f.read())
