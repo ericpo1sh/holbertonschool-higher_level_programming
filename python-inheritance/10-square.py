2578c05a72475f6d1d72608fba6dd2de19e6ef93
ericpo1sh <ericpo1sh@gmail.com>
2023-09-25 08:50:08 -0700

    Fixed whitesapcing and blank lines

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -2,6 +2,7 @@
 """ Square Module """
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
     """ Initializing the Square class """
     pass
@@ -9,8 +10,10 @@ class Square(Rectangle):
     def __init__(self, size):
         """ Validating with integer_validator """
         self.integer_validator("size", size)
+        """ setting square to take in two parameters """
         super().__init__(size, size)
         self.__size = size
-    
+
     def area(self):
+        """ returning the area """
         return self.__size ** 2
