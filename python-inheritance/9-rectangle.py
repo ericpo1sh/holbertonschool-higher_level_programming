d6c9d05d77b7c4967be211fe05ef77e294f01095
ericpo1sh <ericpo1sh@gmail.com>
2023-09-25 08:38:25 -0700

    Fixed area function

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -8,13 +8,13 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """ Validating with integer_validator """
-        super().integer_validator("width", width)
+        self.integer_validator("width", width)
         self.__width = width
-        super().integer_validator("height", height)
+        self.integer_validator("height", height)
         self.__height = height
 
     def area(self):
-        return self.__width ** 2
+        return self.__width * self.__height
 
     def __repr__(self):
         """ Print function that displays x/y """
