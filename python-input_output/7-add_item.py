6527d2d220c6ec165f6ae47d5ad0bd17da34388b
ericpo1sh <ericpo1sh@gmail.com>
2023-09-25 14:26:53 -0700

    test

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ 7-add_iem.py Module """
 from sys import argv
-save = __import__('5-save_to_json_file').save_to_json_file
-load = __import__('6-load_from_json_file').load_from_json_file
+Jsave = __import__('5-save_to_json_file').save_to_json_file
+Jload = __import__('6-load_from_json_file').load_from_json_file
 
 
 try:
-    py_list = load("add_item.json")
+    py_list = Jload("add_item.json")
 except FileNotFoundError:
     py_list = []
-    save(py_list + argv[1:], "add_item.json")
+    Jsave(py_list + argv[1:], "add_item.json")
