c7142e77a742eab5bcd5e1e31cde9791b43d55f3
ericpo1sh <ericpo1sh@gmail.com>
2023-09-25 12:43:26 -0700

    tadded else statement

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -6,6 +6,6 @@ def append_write(filename="", text=""):
     """ initializing the append_write function """
     if not open(filename, 'r'):
         exit
-
+    
     with open(filename, 'a') as f:
         return f.write(text)
