170f056667f373c1331d7c4796947616312e2967
ericpo1sh <ericpo1sh@gmail.com>
2023-09-25 14:25:02 -0700

    test

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """ 7-add_iem.py Module """
 from sys import argv
-
-
 save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
+
 
 try:
     py_list = load("add_item.json")
