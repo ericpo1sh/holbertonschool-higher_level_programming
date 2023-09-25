e7625ebe04c8fc9e629ae22e63c590875fbd1a1d
ericpo1sh <ericpo1sh@gmail.com>
2023-09-25 14:16:22 -0700

    test

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,14 +2,14 @@
 import sys
 """ 7-add_iem.py """
 
-if __name__ == '__main__':
-    save_to_json_file = \
-        __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        PythonList = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        Pythonlist = []
-    save_to_json_file(PythonList + sys.argv[1:], "add_item.json")
+save_to_json_file = \
+    __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = \
+    __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    PythonList = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    Pythonlist = []
+save_to_json_file(PythonList + sys.argv[1:], "add_item.json")
