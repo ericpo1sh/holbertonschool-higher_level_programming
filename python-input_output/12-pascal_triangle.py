90a9d897d478994d1d092b1108f7061d85452c40
ericpo1sh <ericpo1sh@gmail.com>
2023-09-26 10:34:37 -0700

    12. Pascal's Triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -13,13 +13,15 @@ def pascal_triangle(n):
     for i in range(1, n):
         row = []
         for j in range(0, i+1):
-            if j == 0 or j ==i:
+            if j == 0 or j == i:
                 row.append(1)
             else:
                 prev = rows[i - 1]
                 row.append(prev[j - 1] + prev[j])
         rows.append(row)
     return rows
+
+
 if __name__ == '__main__':
     result = pascal_triangle(n)
     print(result)
