adf5125a8a260163a8ce86f179e91ddb56ea1962
ericpo1sh <ericpo1sh@gmail.com>
2023-09-26 08:15:23 -0700

    test

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def load_from_json_file(filename):
     """ initializing the function """
-    with open(filename) as f:
+    with open(filename, 'r') as f:
         return json.load(f)
--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ 7-add_item.py Module """
 from sys import argv
-import json
 Jsave = __import__('5-save_to_json_file').save_to_json_file
 Jload = __import__('6-load_from_json_file').load_from_json_file
 
