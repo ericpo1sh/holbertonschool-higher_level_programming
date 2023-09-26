4b77728730acb4462545050ff69a51a8440d9932
ericpo1sh <ericpo1sh@gmail.com>
2023-09-26 10:48:21 -0700

    test

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" 7-add_item.py Module """
+from sys import argv
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
+
+
+try:
+    py_list = load('add_item.json')
+except FileNotFoundError:
+    py_list = []
+finally:
+    save(py_list + argv[1:], 'add_item.json')
