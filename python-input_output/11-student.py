ecd57db30f20075962444d84a2bd9d97a464f5ff
ericpo1sh <ericpo1sh@gmail.com>
2023-09-26 09:38:53 -0700

    test

---
+++ python-input_output/11-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" 9-student module """
+
+
+class Student:
+    """ Student Class initiated """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ returns dictionary representation of a student """
+        if type(attrs) is list and all(type(item) == str for item in attrs):
+            new_dict = {}
+            for aTT in attrs:
+                if hasattr(self, aTT):
+                    new_dict[aTT] = getattr(self, aTT)
+            return new_dict
+        else:
+            return self.__dict__
--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """ 7-add_item.py Module """
 from sys import argv
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
 
 
-Jsave = __import__('5-save_to_json_file').save_to_json_file
-Jload = __import__('6-load_from_json_file').load_from_json_file
 try:
-    py_list = Jload('add_item.json')
+    py_list = load('add_item.json')
 except FileNotFoundError:
     py_list = []
 finally:
-    Jsave(py_list + argv[1:], 'add_item.json')
+    save(py_list + argv[1:], 'add_item.json')
