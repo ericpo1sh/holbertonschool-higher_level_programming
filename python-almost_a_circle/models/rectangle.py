eeb5e17c73baf83663d20191a6bf950e5da1723b
ericpo1sh <ericpo1sh@gmail.com>
2023-09-28 12:02:06 -0700

    changed if conditiona

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -21,7 +21,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """ Initializing characteristics of widh """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value <= 0:
             raise ValueError("width must be > 0")
@@ -36,7 +36,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """ Setting characteristics of height """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value <= 0:
             raise ValueError("height must be > 0")
@@ -51,7 +51,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """ Setting characteristics for x """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("x must be an integer")
         elif value <= 0:
             raise ValueError("x must be > 0")
@@ -66,7 +66,7 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """ Setting characteristics of y """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("y must be an integer")
         elif value <= 0:
             raise ValueError("y must be > 0")
