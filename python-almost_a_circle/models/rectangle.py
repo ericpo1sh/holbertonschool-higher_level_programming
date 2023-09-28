c6f14c7726f4c14bf41930f3359cecb5d6f3369d
ericpo1sh <ericpo1sh@gmail.com>
2023-09-28 12:00:18 -0700

    Update the class Rectangle by adding validation of all setter methods and instantiation (id excluded):

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -21,7 +21,12 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """ Initializing characteristics of widh """
-        self.__width = value
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
 
     @property
     def height(self):
@@ -31,7 +36,12 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """ Setting characteristics of height """
-        self.__height = value
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = value
 
     @property
     def x(self):
@@ -41,7 +51,12 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """ Setting characteristics for x """
-        self.__x = value
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        elif value <= 0:
+            raise ValueError("x must be > 0")
+        else:
+            self.__x = value
 
     @property
     def y(self):
@@ -51,4 +66,9 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """ Setting characteristics of y """
-        self.__y = value
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        elif value <= 0:
+            raise ValueError("y must be > 0")
+        else:
+            self.__y = value
