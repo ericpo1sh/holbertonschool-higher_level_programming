9973170a6f911be14361d502936bdce7e4e2266f
ericpo1sh <ericpo1sh@gmail.com>
2023-09-29 09:23:27 -0700

    test

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -46,8 +46,8 @@ class Base:
     def create(cls, **dictionary):
         """ Function that returns instances of all attributes """
         if cls.__name__ == "Rectangle":
-            dum = cls(1, 1, 1)
+            dum = cls(5, 5, 5)
         elif cls.__name__ == "Sqaure":
-            dum = cls(1, 1, 1)
+            dum = cls(5, 5, 5)
         dum.update(**dictionary)
         return dum
