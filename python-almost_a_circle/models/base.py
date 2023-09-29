3bbe147f5fb855662d26327293db56fa847cce1e
ericpo1sh <ericpo1sh@gmail.com>
2023-09-29 07:42:05 -0700

    Updated conditional

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -18,7 +18,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ Function that returns dictionary representation """
-        if not list_dictionaries or list_dictionaries is None:
+        if list_dictionaries == [] or list_dictionaries is None:
             return []
         else:
             return json.dumps(list_dictionaries)
