d0373a8e6dd2e4b54cac4cb752bd113f9512f254
ericpo1sh <ericpo1sh@gmail.com>
2023-09-29 08:56:41 -0700

    test

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -21,8 +21,7 @@ class Base:
         """ Function that returns dictionary representation """
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
-        else:
-            return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
