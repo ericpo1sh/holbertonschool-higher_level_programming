d60b5be76f31ad2ba8c6ffcb1699761c21736313
ericpo1sh <ericpo1sh@gmail.com>
2023-09-29 07:54:55 -0700

    testing

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -18,7 +18,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ Function that returns dictionary representation """
-        if len(list_dictionaries) == 0 or list_dictionaries is None:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
