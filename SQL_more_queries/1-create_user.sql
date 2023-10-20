1dbbe89e84b7d9b07468e1e0fb5c3fbf3ce7bc4e
ericpo1sh <ericpo1sh@gmail.com>
2023-10-20 13:03:57 -0500

    Write a script that creates the MySQL server user user_0d_1.

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Write a script that creates the MySQL server user user_0d_1.
 IF NOT EXIST CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES TO 'user_od_1'@'localhost';
+GRANT ALL PRIVILEGES *.* TO 'user_od_1'@'localhost';
