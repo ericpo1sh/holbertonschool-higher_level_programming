4d3103d633b01fcd8e6af8165756d2a83a39e9cc
ericpo1sh <ericpo1sh@gmail.com>
2023-10-20 13:22:22 -0500

    Write a script that creates the database hbtn_0d_2 and the user user_0d_2.

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- Write a script that creates the database hbtn_0d_2 and the user user_0d_2. 
 CREATE DATABASE IF NOT EXIST 'hbtn_0d_2';
 CREATE USER IF NOT EXIST 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON `hbtn_0d_2` TO 'user_0d_2'@'localhost';
+GRANT SELECT ON 'hbtn_0d_2' TO 'user_0d_2'@'localhost';
