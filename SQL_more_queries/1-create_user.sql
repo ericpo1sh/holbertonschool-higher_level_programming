4d488c2a302089704b4f7edaa931c5de58c7e838
ericpo1sh <ericpo1sh@gmail.com>
2023-10-20 12:56:44 -0500

    Fixed Grants

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- Write a script that creates the MySQL server user user_0d_1.
-IF NOT EXIST CREATE USER 'user_0d_1'@'localhost' GRANT ALL IDENTIFIED BY 'user_0d_1_pwd';
+IF NOT EXIST CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES to 'user_od_1'@'localhost'
