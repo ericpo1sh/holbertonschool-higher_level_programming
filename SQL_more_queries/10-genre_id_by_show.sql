c15adad5632d5b6dff89feddaa62890df2b5a4e2
ericpo1sh <ericpo1sh@gmail.com>
2023-10-23 12:09:01 -0500

    Import the database dump from hbtn_0d_tvshows to your MySQL server

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -2,6 +2,6 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_shows_genres
-ON tv_shows_genres.show_id = tv_shows.id
-WHERE tv_shows_genres.genre_id IS NOT NULL
-ORDER BY tv_shows.title ASC, tv_shows_genres.genre_id ASC;
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.genre_id IS NOT NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
