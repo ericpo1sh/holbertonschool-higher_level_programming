d638c05f9ece6282f2b929dfe9217cbff08ed84c
ericpo1sh <ericpo1sh@gmail.com>
2023-10-23 14:18:44 -0500

    Write a script that lists all Comedy shows in the database hbtn_0d_tvshows.

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,5 @@
 -- Write a script that lists all Comedy shows in the database hbtn_0d_tvshows.
-SELECT tv_shows.name 
+SELECT tv_shows.title
 FROM tv_genres
 JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
