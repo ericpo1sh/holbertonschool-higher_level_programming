30800ab8db4086470f544612ad086de32deed919
ericpo1sh <ericpo1sh@gmail.com>
2023-10-24 14:25:10 -0500

    test

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
         FROM cities \
         JOIN states \
         ON cities.state_id = states.id \
-        ORDER BY cities.id ASC\
+        ORDER BY cities.name ASC\
         "
     )
     output = database_cursor.fetchall()
