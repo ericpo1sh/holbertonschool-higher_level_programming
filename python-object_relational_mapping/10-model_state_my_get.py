a89e5dfa9e4e3f42f744044e35276c04ab12161c
Travis Adamson <tndadamson@gmail.com>
2023-10-24 14:42:03 -0500

    Fixing output

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -21,6 +21,6 @@ if __name__ == "__main__":
     if state is not None:
         print("{}".format(state.id))
     else:
-        print("Not Found")
+        print("Not found")
 
     session.close()
