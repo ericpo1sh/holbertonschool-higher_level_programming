201f71e61c9c9bb4623947a783cf53b1bd85d161
ericpo1sh <ericpo1sh@gmail.com>
2023-10-24 11:59:32 -0500

    using matts to see if i pass checker

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python3
 """ Write a script that lists all states from the database hbtn_0e_0_usa: """
 import MySQLdb
-from sys import argv
+import sys
 
 
-def list_states():
-    """ Selects each state from database """
+def select_states():
+    """Grabs states from database"""
 
-    username = argv[1]
-    passwd = argv[2]
-    database = argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
     db = MySQLdb.connect(host='localhost',
                          port=3306,
                          user=username,
-                         passwd=passwd,
+                         passwd=password,
                          db=database)
-    cursor = db.cursor()
-    cursor.execute("SELECT states.name FROM states ORDER BY states.id ASC")
-    states = cursor.fetchall()
-    for statename in states:
-        print(statename)
-
-    cursor.close()
+    database_cursor = db.cursor()
+    database_cursor.execute('SELECT * FROM states ORDER BY id ASC')
+    states = database_cursor.fetchall()
+    for state in states:
+        print(state)
+    database_cursor.close()
     db.close()
 
 
 if __name__ == "__main__":
-    list_states()
+    select_states()
