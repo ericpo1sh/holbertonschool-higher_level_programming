7151b4e2264d2e454219bdcfde68108a93f009f8
ericpo1sh <ericpo1sh@gmail.com>
2023-10-24 13:25:31 -0500

    Testing permissions

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Write a script that lists all states with a name starting with N"""
+"""Write a script that lists all states with a name starting with N """
 import MySQLdb
 from sys import argv
 
