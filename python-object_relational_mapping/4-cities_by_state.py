5a3bf4ef5a8c090d9dcba75e9a4a482905a88a38
ericpo1sh <ericpo1sh@gmail.com>
2023-10-24 13:59:35 -0500

    Write a script that lists all cities from the database hbtn_0e_4_usa

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -23,7 +23,8 @@ if __name__ == "__main__":
     database_cursor = db.cursor()
     database_cursor.execute(
         "\
-        SELECT * FROM cities JOIN states \
+        SELECT cities.id, cities.name, states.name \
+        FROM cities JOIN states \
         ON cities.state_id = states.id \
         ORDER BY cities.id ASC\
         "
