c1e438402bcf3af443815db693126c01e483546b
ericpo1sh <ericpo1sh@gmail.com>
2023-10-24 23:31:02 -0500

    Write a Python file similar to model_state.py named model_city.py that contains the class definition of a City.

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Write a Python file similar to model_state.py named model_city.py
+that contains the class definition of a City. """
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True
+    )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    cities = session.query(City, State).join(State).all()
+    for city, state in cities:
+        print(f'{state.name}: ({city.id}) {city.name}')
+    session.commit()
