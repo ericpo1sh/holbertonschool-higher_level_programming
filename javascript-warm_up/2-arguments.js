062401aa5f19c9863be32964033fbc720633c647
ericpo1sh <ericpo1sh@gmail.com>
2023-11-23 22:21:17 -0600

    fixed typo

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 // Write a script that prints message when arg passed
 if (process.argv.length >= 3) {
-    console.log("Arguments found")
-}
-else {
-    console.log("No argument")
+  console.log('Argument found');
+} else {
+  console.log('No argument');
 }
