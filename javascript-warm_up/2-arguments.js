8e0fdae87530677c96e391292301426b381b8c93
ericpo1sh <ericpo1sh@gmail.com>
2023-11-23 22:20:26 -0600

    // Write a script that prints message when arg passed

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Write a script that prints message when arg passed
+if (process.argv.length >= 3) {
+    console.log("Arguments found")
+}
+else {
+    console.log("No argument")
+}
