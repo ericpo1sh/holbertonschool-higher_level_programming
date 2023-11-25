23e3986fe93fba02cd2df90a893e4bcc8352fe50
ericpo1sh <ericpo1sh@gmail.com>
2023-11-24 21:56:26 -0600

    Write a script that prints two args passed + "is"

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -2,7 +2,6 @@
 // Write a script that prints two args passed + "is"
 if (process.argv[2] && process.argv[3]) {
   console.log(process.argv[2] + ' is ' + process.argv[3]);
-}
-else {
-  console.log(process.argv[2] + " is undefined");
+} else {
+  console.log(process.argv[2] + ' is undefined');
 }
