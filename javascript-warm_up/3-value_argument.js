78e5e8ea3a37ebb524b218f4b6762393691c1211
ericpo1sh <ericpo1sh@gmail.com>
2023-11-24 21:51:18 -0600

    fixed 3

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
 // Write a script that prints the arg thats passes
-if (process.argv[2] && process.argv[3]) {
-  console.log(process.argv[2] + ' ' + process.argv[3]);
-}
-else if (process.argv[2]) {
+if (process.argv[2]) {
   console.log(process.argv[2]);
 } else {
   console.log('No argument');
