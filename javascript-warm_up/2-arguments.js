f39a8bd2ad1e9e898949828eec8c8db379ca37b9
ericpo1sh <ericpo1sh@gmail.com>
2023-11-24 20:38:37 -0600

    fixed semistandard

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 // Write a script that prints message when arg passed
-if (process.argv.length == 3) {
+if (process.argv.length === 3) {
   console.log('Argument found');
-}
-else if (process.argv.length > 3) {
+} else if (process.argv.length > 3) {
   console.log('Arguments found');
-} 
-else {
+} else {
   console.log('No argument');
 }
