db9115ab87cf3ff57aaef1320a810a43f20c1f48
ericpo1sh <ericpo1sh@gmail.com>
2023-11-25 19:52:07 -0600

    fixed output to strings

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
 // Wtire a script that prints if first arg is a int
+// Converting argv[2] into a number
 const number = Number(process.argv[2]);
-const numb = number | 0;
-if (!isNaN(numb)) {
+// checking if it is a number
+if (!isNaN(number)) {
+// remove decimals from the number and print
+  const numb = number | 0;
   console.log('My number: ' + numb);
 } else {
   console.log('Not a number');
