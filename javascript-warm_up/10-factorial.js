07073ac3d84fc896a85caa55bc14e752d400fa5d
ericpo1sh <ericpo1sh@gmail.com>
2023-11-26 16:16:11 -0600

    Writing a scrpt that returns the factorial of input

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Writing a scrpt that returns the factorial of input
+function factorial (number) {
+  if (number === 0 || number === 1) {
+    return 1; // return 1 if number is 0-1
+  } else {
+    return number * factorial(number - 1); // Recursively call factorial function
+  }
+}
+const number = Number(process.argv[2]);
+console.log(factorial(number));
