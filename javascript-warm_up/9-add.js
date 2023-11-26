5bf219c7a2e3e858240f2b95e4048dab7b198f15
ericpo1sh <ericpo1sh@gmail.com>
2023-11-26 14:46:08 -0600

    make a function that adds two parameters

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// make a function that adds two parameters
+function add(a, b) {
+  return a + b;
+}
+const number1 = Number(process.argv[2]);
+const number2 = Number(process.argv[3]);
+console.log(add(number1, number2));
