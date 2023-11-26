4ea3413664480124bb5ab403d8f11e1bbd011579
ericpo1sh <ericpo1sh@gmail.com>
2023-11-25 20:33:33 -0600

    fixed semistandard

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,11 +2,11 @@
 // Write a script that prints x times “C is fun”
 const number = Number(process.argv[2]);
 if (process.argv.length === 3) {
-    if (number > 0) {
-        for (let x = 1; x <= number; x++) {
-        console.log('C is fun');
-        }
+  if (number > 0) {
+    for (let x = 1; x <= number; x++) {
+      console.log('C is fun');
     }
+  }
 } else if (process.argv.length < 3) {
-    console.log("Missing number of occurrences")
+  console.log('Missing number of occurrences');
 }
