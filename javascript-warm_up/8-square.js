c3d68d4105b43ed8e87be2b22184de8f4a534b1d
ericpo1sh <ericpo1sh@gmail.com>
2023-11-25 20:58:30 -0600

    Write a script that prints a square

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// Write a script that prints a square
+const number = Number(process.argv[2]);
+if (process.argv.length === 3) {
+  if (number > 0) {
+    for (let y = 1; y <= number; y++) {
+      let set = '';
+      for (let x = 1; x <= number; x++) {
+        set += 'X';
+      }
+      console.log(set)
+    }
+  }
+} else {
+  console.log('Missing size');
+}
