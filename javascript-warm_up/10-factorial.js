9135115a39f3e8f2da6e7835eac3220776878063
ericpo1sh <ericpo1sh@gmail.com>
2023-11-27 10:44:54 -0600

    test

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // Writing a scrpt that returns the factorial of input
 function factorial (number) {
-  if (number <=  1) {
-    return 1; // return 1 if number is 0-1
-  } else {
+  if (number < 0) {
+    return -1; // return 1 if number is 0-1
+  } else if (number > 1) {
     return number * factorial(number - 1); // Recursively call factorial function
   }
 }
