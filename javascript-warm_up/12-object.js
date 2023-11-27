5189f35796a4406710e55f14958cf131844326a0
ericpo1sh <ericpo1sh@gmail.com>
2023-11-27 11:34:21 -0600

    fixed semitandard

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // Update this script to replace the value 12 with 89:
 const myObject = {
-    type: 'object',
-    value: 12
-  };
-  console.log(myObject);
-  // MY CODE HERE
-  myObject.value = 89;
-  // MY CODE UP THERE
-  console.log(myObject);
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+// MY CODE HERE
+myObject.value = 89;
+// MY CODE UP THERE
+console.log(myObject);
