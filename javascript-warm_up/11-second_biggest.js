a4e2c548b048b0ce6f4f3d1790bebb28ab6aa1b0
ericpo1sh <ericpo1sh@gmail.com>
2023-11-27 11:20:20 -0600

    Searches the second biggest integer in the list of arguments.

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Searches the second biggest integer in the list of arguments.
+if (process.argv.length < 3 || process.argv.length === 3) {
+  console.log('0');
+} else {
+  const array = process.argv.slice(2).sort((b, a) => a - b);
+  console.log(array[0]);
+}
