3ce0a5305b716231bf272eefffe1c0f70a58e2f6
ericpo1sh <ericpo1sh@gmail.com>
2023-11-30 12:15:23 -0600

    Created function that reads and prints the content of a file

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Write a script that reads and prints the content of a file.
+
+const fs = require('fs').promises;
+async function readme(filename) {
+  try {
+    const data = await fs.readFile(filename, 'utf-8');
+    console.log(data.toString());
+  } catch (error) {
+    console.error(error);
+  }
+}
+readme(process.argv[2]);
