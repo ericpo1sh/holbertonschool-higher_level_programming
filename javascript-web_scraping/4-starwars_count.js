1f821cdb5f09e12d70db53e7b6f75e04a5d8e3c6
ericpo1sh <ericpo1sh@gmail.com>
2023-12-01 10:59:15 -0600

    test

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// Write a script that prints the title of a Star Wars movie
+// where the episode number matches a given integer.
+const request = require('request');
+async function starWarsCount (url) {
+  let count = 0;
+  request.get(url, (error, response, body) => {
+    const rizzults = JSON.parse(body).results;
+    if (error) console.log(error);
+    else if (response.statusCode === 404) console.log(`code: ${response.statusCode}`);
+    else if (body) {
+      for (film of rizzults) {
+        for (character of film.characters) {
+          if (character.match('https://swapi-api.hbtn.io/api/people/18/')) {
+            count += 1;
+          }
+        }
+      }
+      console.log(count);
+    }
+  });
+}
+starWarsCount(process.argv[2]);
